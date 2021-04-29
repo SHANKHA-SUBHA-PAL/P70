d87f522c8205b858b2049f118700c25c97c778e2
SHANKHA-SUBHA-PAL <subhankarpal76@gmail.com>
2021-04-29 12:24:31 +0530

    CREATED WRITE SCREEN

--- Screens/WriteStory.js
+++ Screens/WriteStory.js
@@ -1,15 +1,58 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Header, TouchableOpacity } from 'react-native';
 
 export default class WriteStory extends React.Component{
 render(){
     return(
         <View>
-            <Text>
-                HI THIS IS WRITING SECTION
-            </Text>
+            <TextInput style={styles.formTextInput}
+            placeholder="Enter Book Name"/>
+            <TextInput style={styles.formTextInput}
+            placeholder="Enter Author Name"/>
+            <TextInput style={styles.formTextInput}
+            placeholder="Enter Book Name"
+            multiline
+            numberOfLines={100}
+            />
+            <TouchableOpacity>
+                <Text>SUBMIT</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 }
+
+const styles = StyleSheet.create({
+
+    keyBoardStyle: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    formTextInput: {
+        width: "75%",
+        height: 35,
+        alignSelf: 'center',
+        borderColor: '#ffab91',
+        borderRadius: 10,
+        borderWidth: 1,
+        marginTop: 20,
+        padding: 10,
+    },
+    button: {
+        width: "75%",
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        backgroundColor: "#ff5722",
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 8, },
+        shadowOpacity: 0.44,
+        shadowRadius: 10.32,
+        elevation: 16,
+        marginTop: 20
+    },
+
+})
